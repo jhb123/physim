924489b37bd51b4823e15a4e0bcd33444cfa1fb8
jhb123 <jhbriggs23@gmail.com>
2025-04-04 22:40:56 +0100

    add toml file configuration files

--- physim-core/src/main.rs
+++ physim-core/src/main.rs
@@ -5,12 +5,36 @@ use physim_core::pipeline::Pipeline;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
-    let mut args = env::args();
-    args.next().ok_or("No pipeline description")?;
 
-    let desc: String = args.intersperse(" ".to_string()).collect();
-    // "cube n=10000 seed=1 ! star mass=10000.0 x=0.5 y=0.5 radius=0.1 ! star mass=10000.0 x=-0.5 y=-0.5 radius=0.1 ! astro theta=1.5 ! glrender "
-    let pipeline = Pipeline::new_from_description(&desc)?;
+    let mut args = env::args().peekable();
+    args.next().ok_or("No configuration provided")?;
+
+    let help_text = include_str!("help.txt");
+
+    let pipeline = if let Some(v) = args.peek() {
+        match v.as_str() {
+            "-h" | "--help" => {
+                println!("{}", help_text);
+                return Ok(());
+            }
+            "-v" | "--version" => {
+                println!("{}", env!("CARGO_PKG_VERSION"));
+                return Ok(());
+            }
+            "-f" | "--file" => {
+                args.next();
+                let file = args.next().ok_or("No file provided")?;
+                Pipeline::new_from_file(&file)?
+            }
+            _ => {
+                let desc: String = args.intersperse(" ".to_string()).collect();
+                Pipeline::new_from_description(&desc)?
+            }
+        }
+    } else {
+        println!("{}", help_text);
+        return Ok(());
+    };
 
     pipeline.run();
 
