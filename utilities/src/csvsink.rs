a29ff51a92ff01982b3cefb43418f72b5657f74b
jhb123 <jhbriggs23@gmail.com>
2025-07-31 21:36:15 +0100

    (plugin): Make utility plugin and csvsink element
    The utility plugin is for keeping elements which are useful for lots of
    different types of simulation. This currently has one element called
    csvsink, and this element is for writing simulation data out to a csv
    file for processing in graphing software.

---
+++ utilities/src/csvsink.rs
@@ -0,0 +1,94 @@
+use physim_attribute::render_element;
+use physim_core::{
+    Entity,
+    messages::MessageClient,
+    plugin::{Element, ElementCreator, render::RenderElement},
+};
+use serde_json::Value;
+use std::io::Write;
+use std::{
+    collections::HashMap,
+    fs::File,
+    sync::atomic::{AtomicUsize, Ordering},
+};
+
+#[render_element(
+    name = "csvsink",
+    blurb = "Output entity position to comma spaced values"
+)]
+struct CsvSink {
+    iteration: AtomicUsize,
+    print_n: usize,
+    file: String,
+}
+
+impl ElementCreator for CsvSink {
+    fn create_element(properties: HashMap<String, Value>) -> Box<Self> {
+        let print_n = properties
+            .get("print_n")
+            .and_then(|v| v.as_u64())
+            .unwrap_or(1) as usize;
+        let file = properties
+            .get("file")
+            .and_then(|v| v.as_str())
+            .unwrap_or("csvsink.csv");
+        Box::new(CsvSink {
+            iteration: AtomicUsize::new(0),
+            print_n,
+            file: String::from(file),
+        })
+    }
+}
+
+impl RenderElement for CsvSink {
+    fn render(
+        &self,
+        _config: physim_core::UniverseConfiguration,
+        state_recv: std::sync::mpsc::Receiver<Vec<Entity>>,
+    ) {
+        let mut file = File::options()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&self.file)
+            .unwrap();
+
+        if let Ok(state) = state_recv.recv() {
+            self.iteration.fetch_add(1, Ordering::Relaxed);
+            print_state(&mut file, state);
+        }
+
+        while let Ok(state) = state_recv.recv() {
+            let iteration = self.iteration.fetch_add(1, Ordering::Relaxed);
+            if iteration.rem_euclid(self.print_n) == 0 {
+                print_state(&mut file, state);
+            }
+        }
+    }
+}
+
+fn print_state(mut file: &mut File, state: Vec<Entity>) {
+    for entity in &state {
+        write!(&mut file, "{},{},{},", entity.x, entity.y, entity.z);
+    }
+    writeln!(&mut file);
+}
+
+impl Element for CsvSink {
+    fn set_properties(&self, _new_props: HashMap<String, Value>) {}
+
+    fn get_property(&self, _prop: &str) -> Result<Value, Box<dyn std::error::Error>> {
+        Err("No property".into())
+    }
+
+    fn get_property_descriptions(
+        &self,
+    ) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+        Ok(HashMap::from([(
+            String::from("print_n"),
+            String::from("print every n iterations"),
+        )]))
+    }
+}
+
+impl MessageClient for CsvSink {}
---
+++ utilities/src/lib.rs
@@ -0,0 +1,5 @@
+mod csvsink;
+
+use physim_core::register_plugin;
+
+register_plugin!("csvsink");
