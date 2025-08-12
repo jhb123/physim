26b92b3e2789a01f90a0bbff396f5fda17518b11
jhb123 <jhbriggs23@gmail.com>
2025-08-12 10:59:46 +0100

    element: Add wrapper element

--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -1,6 +1,7 @@
 mod bbox;
 mod csvsink;
+mod wrapper;
 
 use physim_core::register_plugin;
 
-register_plugin!("csvsink", "bbox");
+register_plugin!("csvsink", "bbox", "wrapper");
---
+++ utilities/src/wrapper.rs
@@ -0,0 +1,82 @@
+use std::collections::HashMap;
+
+use physim_attribute::transmute_element;
+use physim_core::{
+    Entity,
+    messages::MessageClient,
+    plugin::{Element, ElementCreator, transmute::TransmuteElement},
+};
+use serde_json::Value;
+
+#[transmute_element(
+    name = "wrapper",
+    blurb = "Define a cyclical boundary for the universe"
+)]
+struct Wrapper {
+    xlim: f64,
+    ylim: f64,
+    zlim: f64,
+}
+
+impl TransmuteElement for Wrapper {
+    fn transmute(&self, data: &mut Vec<Entity>) {
+        for e in data.iter_mut() {
+            if e.x.abs() > self.xlim {
+                e.x *= -1.0
+            }
+            if e.y.abs() > self.ylim {
+                e.y *= -1.0
+            }
+            if e.z.abs() > self.zlim {
+                e.z *= -1.0
+            }
+        }
+    }
+}
+
+impl MessageClient for Wrapper {}
+
+impl ElementCreator for Wrapper {
+    fn create_element(props: HashMap<String, Value>) -> Box<Self> {
+        let xlim = props
+            .get("xlim")
+            .map(|x| x.as_f64().unwrap_or(1.0))
+            .unwrap_or(1.0);
+        let ylim = props
+            .get("ylim")
+            .map(|x| x.as_f64().unwrap_or(1.0))
+            .unwrap_or(1.0);
+        let zlim = props
+            .get("zlim")
+            .map(|x| x.as_f64().unwrap_or(1.0))
+            .unwrap_or(1.0);
+        Box::new(Self { xlim, ylim, zlim })
+    }
+}
+
+impl Element for Wrapper {
+    fn set_properties(&self, _new_props: HashMap<String, Value>) {}
+
+    fn get_property(&self, _prop: &str) -> Result<Value, Box<dyn std::error::Error>> {
+        Err("No property".into())
+    }
+
+    fn get_property_descriptions(
+        &self,
+    ) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+        Ok(HashMap::from([
+            (
+                "xlim".to_string(),
+                "Maximum distance from origin in x".to_string(),
+            ),
+            (
+                "ylim".to_string(),
+                "Maximum distance from origin in y".to_string(),
+            ),
+            (
+                "zlim".to_string(),
+                "Maximum distance from origin in z".to_string(),
+            ),
+        ]))
+    }
+}
