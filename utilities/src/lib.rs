7b2fda5373a22b74b597c2315a6f3a12e3f6f2e1
jhb123 <jhbriggs23@gmail.com>
2025-08-16 16:07:39 +0100

    feat: Added idset utility element
    Adds a transmute element which assigns a unique id to every element with
    an id of 0.

--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -1,7 +1,8 @@
 mod bbox;
 mod csvsink;
+mod idset;
 mod wrapper;
 
 use physim_core::register_plugin;
 
-register_plugin!("csvsink", "bbox", "wrapper");
+register_plugin!("csvsink", "bbox", "wrapper", "idset");
