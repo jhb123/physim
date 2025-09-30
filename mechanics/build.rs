9e64b29b2cc5974fdc42e4a1464cfd48b4b6b957
jhb123 <jhbriggs23@gmail.com>
2025-09-30 22:11:16 +0100

    refactor: replace unwraps with expects in build script

--- mechanics/build.rs
+++ mechanics/build.rs
@@ -1,8 +1,9 @@
 use rustc_version::version;
 
 fn main() {
-    let rustc_version = version().unwrap();
-    let target = std::env::var("TARGET").unwrap(); // always set by Cargo
+    let rustc_version = version().expect("Failed to get rustc version");
+    let target =
+        std::env::var("TARGET").expect("Cargo did not set TARGET (this should never happen)");
     let abi_info = format!("rustc:{}|target:{}", rustc_version, target);
     println!("cargo:rustc-env=ABI_INFO={}", abi_info);
 }
