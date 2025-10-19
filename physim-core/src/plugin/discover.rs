39952e26726c887e4565a2b282c554efd66b9801
jhb123 <jhbriggs23@gmail.com>
2025-10-19 18:19:23 +0100

    feat: warn on plugin build info mismatch

--- physim-core/src/plugin/discover.rs
+++ physim-core/src/plugin/discover.rs
@@ -343,14 +343,11 @@ unsafe fn validate_plugin_abi(lib_path: &str) -> bool {
     // This is basically a hack for C plugins. I think the ABI for
     // C is stable. If you'r making a C plugin, you probably know
     // about all the terrible things that can happen in physim.
-    if rust_version == "C" {
-        return true;
-    }
-    let ret = rust_version == PHYSIM_PLUGIN_LOADER_RUSTC_VERSION;
-    if !ret {
+    let warn = (rust_version == PHYSIM_PLUGIN_LOADER_RUSTC_VERSION) || (rust_version == "C");
+    if !warn {
         eprintln!("{} was built with a different version of the rust compiler or for a different platform. The plugin compiled with {} but physim compiled with {} ",&lib_path, rust_version,PHYSIM_PLUGIN_LOADER_RUSTC_VERSION);
     }
-    ret
+    return true;
 }
 
 unsafe fn get_plugin_meta(lib: &Library) -> Vec<ElementMeta> {
